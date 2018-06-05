60613c925b685c39ea5cca47d89adc59d811970a
gillenha <gillen08@gmail.com>
2018-06-05 16:58:51 -0400

    added Redux async actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,33 +5,28 @@ import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	}
 }
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: []
-		}
-	}
-
 	componentDidMount() {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then(response => response.json())
-			.then(users => {this.setState({ robots: users})});
-	}
+			this.props.onRequestRobots();
+}
 
 // On search change is coming down as a prop, we don't need to declare it as a method of app.js
 	// onSearchChange = (event) => {
@@ -39,12 +34,11 @@ class App extends Component {
 	// }
 
 	render() {
-		const { robots } = this.state;
-		const { searchField, onSearchChange } = this.props;
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
-		return !robots.length ?
+		return isPending ?
 			<h1>Loading</h1> :
 			(
 			<div className="tc">
